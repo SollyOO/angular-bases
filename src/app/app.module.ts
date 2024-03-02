591b28963190cf2be6f2b858a15802f0445e7cd4
odelacruz <o.delacruz@dataware.com.mx>
2024-03-02 11:48:26 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/components/heroes.module';
+
+import { AppComponent } from './app.component';
 
 
 @NgModule({
